b3f68b54f3718a61385f8f38a4c425dc6fe39ace
Latinbluebarry <eunseong091010@gmail.com>
2026-08-02 16:46:18 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,19 @@
-pluginManagement{repositories{google();mavenCentral();gradlePluginPortal()}}
-dependencyResolutionManagement{repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-repositories{google();mavenCentral()}}
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "Jarvis"
-// include(":app") 삭제 또는 주석 처리
+
+// 주의: include(":app") 문구는 절대 넣지 마세요! (루트 빌드 방식)
